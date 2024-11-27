ee67e117f794bc9a0c1e1f59f3cead36f54de5b6
psyes9 <ethan.a.smith@hotmail.co.uk>
2024-11-27 15:47:29 +0000

    chore: Add initial code for analyzing GP surgery data

---
+++ Hello_World.r
@@ -0,0 +1,31 @@
+# This code defines the variable message and assigns it the value "Hello, World!".
+message <- "Hello, World!"
+# This code prints the value of the variable message to the console.
+print(message)
+
+# Question Framed for my data:
+
+# Question 1: Which GP Surgery that has the highest number of patients registered?
+
+# Question 2: What is the area/disctrict with the highest number of registered GP Surgeries?
+
+# Question 3: What sector has the best ratio of GPs to population?
+
+# Question 4: How many surgeries have 10 or more GPs?
+
+# Data Sources:
+# UK population per postcode 2021-
+#https://www.beta.ons.gov.uk/aboutus/transparencyandgovernance/freedomofinformationfoi/postcodelevelpopulationdatafromcensus2021
+population-postcode-data = "DataSources/uk-population-postcode-21.csv"
+
+# Patient count at each GP Surgery in the UK December 2021-
+# https://digital.nhs.uk/data-and-information/publications/statistical/patients-registered-at-a-gp-practice/december-2021#resources
+patients-registered-data = "DataSources/gp-surgery-patient-count-dec-21.csv"
+
+# GP Practitioners data- 
+# https://digital.nhs.uk/services/organisation-data-service/export-data-files/csv-downloads/gp-and-gp-practice-related-data
+gp-practitioner-data = "DataSources/gp-practitioner-data-dec-21.csv"
+
+# GP Surgery data-
+# https://digital.nhs.uk/services/organisation-data-service/export-data-files/csv-downloads/gp-and-gp-practice-related-data
+gp-surgery-data = "DataSources/gp-surgery-data-24.csv"
