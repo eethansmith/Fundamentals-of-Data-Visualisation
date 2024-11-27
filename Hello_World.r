bdd537a98e2ef787e415d184b72abea8e32eb0ca
psyes9 <ethan.a.smith@hotmail.co.uk>
2024-11-27 16:43:27 +0000

    Clean and update GP surgery data for analysis

--- Hello_World.r
+++ Hello_World.r
@@ -26,23 +26,30 @@ population_postcode <- read.csv("DataSources/uk-population-postcode-21.csv")
 gp_surgery <- read.csv("DataSources/gp-surgery-data-21.csv")
 # Extracting the columns "Organisation_Code" and "Name" from the dataset
 gp_surgery_clean <- gp_surgery %>%
-  select(Organisation_Code = CODE, Name)
+  select(`Organisation Code`, Name)
 
 # Patient count at each GP Surgery in the UK December 2021-
 # https://digital.nhs.uk/data-and-information/publications/statistical/patients-registered-at-a-gp-practice/december-2021#resources
 patients_registered <- read.csv("DataSources/gp-surgery-patient-count-21.csv")
 # Clean data taking global"code", postcode and total patients registered
 clean_data <- patients_registered %>%
-  select(CODE, POSTCODE, NUMBER_OF_PATIENTS)
-
-clean_data <- clean_data %>%
+  select(CODE, POSTCODE, NUMBER_OF_PATIENTS) %>%
   distinct()
 
+# Check structure and content of clean_data
 str(clean_data)      # Check column types
 head(clean_data)     # View the first few rows
 summary(clean_data)  # Get summary statistics
 
-write.csv(clean_data, "clean_gp_surgery_data.csv", row.names = TRUE)
+merged_data <- clean_data %>%
+  left_join(gp_surgery_clean, by = c("CODE" = "Organisation Code"))
+
+str(merged_data)
+head(merged_data)
+summary(merged_data)
+
+write.csv(merged_data, "gp_surgery_data.csv", row.names = TRUE)
+
 
 # GP Practitioners data-
 # https://digital.nhs.uk/services/organisation-data-service/export-data-files/csv-downloads/gp-and-gp-practice-related-data
