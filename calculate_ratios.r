e738dfbee02a496cc1d027267024b97b311bf134
psyes9 <ethan.a.smith@hotmail.co.uk>
2024-12-02 22:08:19 +0000

    Clean and update GP surgery data, merge with population data

--- calculate_ratios.r
+++ calculate_ratios.r
@@ -5,8 +5,12 @@ data <- read.csv("population_by_postcode.csv")
 data$Population_to_Practitioners <- data$Population / data$Registered_Practitioners
 data$Population_to_Surgeries <- data$Population / data$GP_Surgeries
 
-# Save the updated dataframe to a new CSV file (optional)
+# Reorder the columns
+data <- data[, c("Postcode_Area", "Area_Name", "Population", 
+                 "Registered_Patients", "Registered_Practitioners", 
+                 "GP_Surgeries", "Population_to_Surgeries", 
+                 "Population_to_Practitioners")]
+
+# Save the updated dataframe to a new CSV file
 write.csv(data, "updated_population_by_postcode.csv", row.names = FALSE)
 
-# View the updated dataframe
-print(data)
